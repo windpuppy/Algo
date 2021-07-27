07b2c4eaf188282b96e2a40c893294d6c930687a
Kenny Li <Kenny.Li@spacelabs.com>
2021-07-27 12:30:59 -0700

    Cleaned up some printouts.

--- Threading/SharedPointer.h
+++ Threading/SharedPointer.h
@@ -15,27 +15,26 @@ private:
 
 	struct Base
 	{
-		Base() { std::cout << "  Base::Base()\n"; }
+		Base() { std::cout << "  Base::Base()" << endl; }
 		// Note: non-virtual destructor is OK here
-		~Base() { std::cout << "  Base::~Base()\n"; }
+		~Base() { std::cout << "  Base::~Base()" << endl; }
 	};
 
 	struct Derived : public Base
 	{
-		Derived() { std::cout << "  Derived::Derived()\n"; }
-		~Derived() { std::cout << "  Derived::~Derived()\n"; }
+		Derived() { std::cout << "  Derived::Derived()" << endl; }
+		~Derived() { std::cout << "  Derived::~Derived()" << endl; }
 	};
 
 	void static bar(shared_ptr<Base> p)
 	{
 		this_thread::sleep_for(chrono::seconds(1));
-		shared_ptr<Base> lp = p; // thread-safe, even though the
-									  // shared use_count is incremented
+		shared_ptr<Base> lp = p; // thread-safe, even though the shared use_count is incremented
 		{
 			static mutex io_mutex;
 			lock_guard<mutex> lk(io_mutex);
-			cout << "local ptr in a thread: "
-				<< "lp.get() = " << lp.get() << ", lp.use_count() = " << lp.use_count() << '\n';
+			cout << "local ptr in a thread: " << endl;
+			cout << "  lp.get() = " << lp.get() << ", lp.use_count() = " << lp.use_count() << endl;
 		}
 	}
 public:
@@ -61,19 +60,24 @@ public:
 		p1.reset();
 		print_counter(p1.use_count()); // 0
 		print_counter(p3.use_count()); // 1, NOT ZERO!
+
+		cout << endl;
 	}
 
 	void demo_inheritence() {
 		shared_ptr<Base> p = make_shared<Derived>();
 
-		cout << "Created a shared Derived (as a pointer to Base)\n"
-			<< "  p.get() = " << p.get() << ", p.use_count() = " << p.use_count() << '\n';
+		cout << "Created a shared Derived (as a pointer to Base)" << endl;
+		cout << "  p.get() = " << p.get() << ", p.use_count() = " << p.use_count() << endl;
 		thread t1(bar, p), t2(bar, p);
+		
 		p.reset(); // release ownership from main
-		cout << "Shared ownership between 2 threads and released\n"
-			<< "ownership from main:   "
-			<< "p.get() = " << p.get() << ", p.use_count() = " << p.use_count() << '\n';
+		cout << "Shared ownership between 2 threads and released" << endl;
+		cout << "ownership from main" << endl;
+		cout << "  p.get() = " << p.get() << ", p.use_count() = " << p.use_count() << endl;
 		t1.join(); t2.join();
-		cout << "All threads completed, the last one deleted Derived\n";
+		cout << "All threads completed, the last one deleted Derived" << endl;
+
+		cout << endl;
 	}
 };
